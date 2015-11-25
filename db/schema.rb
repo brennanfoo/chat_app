9e699137bb3a6f3ef07f3beb2e114cf919812472
brennanfoo <brennan.foo@gmail.com>
2015-11-25 17:54:44 -0500

    Migrate:db for messages

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151125223653) do
+ActiveRecord::Schema.define(version: 20151125224429) do
 
   create_table "conversations", force: true do |t|
     t.integer  "sender_id"
@@ -23,6 +23,17 @@ ActiveRecord::Schema.define(version: 20151125223653) do
 
   add_index "conversations", ["recipient_id"], name: "index_conversations_on_recipient_id"
   add_index "conversations", ["sender_id"], name: "index_conversations_on_sender_id"
+
+  create_table "messages", force: true do |t|
+    t.text     "body"
+    t.integer  "conversation_id"
+    t.integer  "user_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "messages", ["conversation_id"], name: "index_messages_on_conversation_id"
+  add_index "messages", ["user_id"], name: "index_messages_on_user_id"
 
   create_table "users", force: true do |t|
     t.string   "email",                  default: "", null: false
