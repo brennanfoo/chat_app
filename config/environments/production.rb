b9feec9657d8aa106d83afa1053e9f0de8f6018f
brennanfoo <brennan.foo@gmail.com>
2015-11-25 23:13:28 -0500

    Change log levels to debug in production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -45,8 +45,9 @@ Rails.application.configure do
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   # config.force_ssl = true
 
-  # Set to :debug to see everything in the log.
-  config.log_level = :info
+  # Set to :debug to see everything in the log. 
+  # Set to :info for "We're sorry but something went wrong..."
+  config.log_level = :debug
 
   # Prepend all log lines with the following tags.
   # config.log_tags = [ :subdomain, :uuid ]
